b67675446bdeb3489a3d43f1a170945324059802
Nam Le <leon.lethanhnam@gmail.com>
2022-07-31 15:54:16 +0700

    Auth0_reactjs

---
+++ golang-gin/views/js/app.jsx
@@ -0,0 +1,153 @@
+const AUTH0_CLIENT_ID = "NYG1FjK0U1n7AjcywUHjHUQtJuqpmxFP";
+const AUTH0_DOMAIN = "namle311.jp.auth0.com";
+const AUTH0_CALLBACK_URL = "http://localhost:3000";
+const AUTH0_API_AUDIENCE = "https://namle311.jp.auth0.com/api/v2/";
+
+class App extends React.Component {
+  parseHash() {
+    this.auth0 = new auth0.WebAuth({
+      domain: AUTH0_DOMAIN,
+      clientID: AUTH0_CLIENT_ID
+    });
+    this.auth0.parseHash(window.location.hash, (err, authResult) => {
+      if (err) {
+        return console.log(err);
+      }
+      if (
+        authResult !== null &&
+        authResult.accessToken !== null &&
+        authResult.idToken !== null
+      ) {
+        localStorage.setItem("access_token", authResult.accessToken);
+        localStorage.setItem("id_token", authResult.idToken);
+        localStorage.setItem(
+          "profile",
+          JSON.stringify(authResult.idTokenPayload)
+        );
+        window.location = window.location.href.substr(
+          0,
+          window.location.href.indexOf("#")
+        );
+      }
+    });
+  }
+
+  setup() {
+    $.ajaxSetup({
+      beforeSend: (r) => {
+        if (localStorage.getItem("access_token")) {
+          r.setRequestHeader(
+            "Authorization",
+            "Bearer " + localStorage.getItem("access_token")
+          );
+        }
+      }
+    });
+  }
+   
+
+  setState() {
+    let idToken = localStorage.getItem("id_token");
+    if (idToken) {
+      this.loggedIn = true;
+    } else {
+      this.loggedIn = false;
+    }
+  }
+
+  componentWillMount() {
+    this.setup();
+    this.parseHash();
+    this.setState();
+  }
+
+  render() {
+    if (this.loggedIn) {
+      return <LoggedIn />;
+    }
+    return <Home />;
+  }
+}
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.authenticate = this.authenticate.bind(this);
+  }
+  authenticate() {
+    this.WebAuth = new auth0.WebAuth({
+      domain: AUTH0_DOMAIN,
+      clientID: AUTH0_CLIENT_ID,
+      scope: "openid profile",
+      audience: AUTH0_API_AUDIENCE,
+      responseType: "token id_token",
+      redirectUri: AUTH0_CALLBACK_URL
+    });
+    this.WebAuth.authorize();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-8 col-xs-offset-2 jumbotron text-center">
+            <h1>CMS_QOR</h1>
+            <p>Sign in to get access </p>
+            <a
+              onClick={this.authenticate}
+              className="btn btn-primary btn-lg btn-login btn-block"
+            >
+              Sign In
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class LoggedIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // jokes: []
+    };
+
+    this.serverRequest = this.serverRequest.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  logout() {
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("profile");
+    location.reload();
+  }
+
+  serverRequest() {
+    $.get("http://localhost:3000/admin", res => {
+      this.setState({
+        admin:res
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.serverRequest();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <br />
+        <span className="pull-right">
+          <a onClick={this.logout}>Log out</a>
+        </span>
+        <h2>CMS</h2>
+        <p>Let's get start!!!</p>
+        <a href="http://localhost:3000/admin">CMS_QOR</a>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<App />, document.getElementById("app"));
